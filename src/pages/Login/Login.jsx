446a73c450abc48254b9398b18e06f04ec408ca0
oiiiMansi <sainimansi360@gmail.com>
2025-06-09 09:39:17 +0530

    Add login page layout with background and logo

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './Login.css'
+import assets from '../../assets/assets'
 
 const Login = () => {
   return (
-    <div>
-        
+    <div className='login'>
+        <img src={assets.logo_big} alt="" className='logo' />
+
     </div>
   )
 }
