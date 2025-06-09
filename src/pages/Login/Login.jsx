e8f989fdf5e5e3c94e114419d82f2e919f27512b
oiiiMansi <sainimansi360@gmail.com>
2025-06-09 10:04:37 +0530

    Add signup form with input fields and terms agreement checkbox

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -6,6 +6,18 @@ const Login = () => {
   return (
     <div className='login'>
         <img src={assets.logo_big} alt="" className='logo' />
+        <form className='login-form'>
+            <h2>Sign Up</h2>
+            <input type="text" placeholder='username' className="form-input" required />
+            <input type="email" placeholder='Email address' className="form-input" />
+            <input type="password" placeholder='password' className="form-input" />
+            <button type='submit'>Sign Up</button>
+
+            <div className="login-term">
+                <input type="checkbox" />
+                <p>I agree to the Terms and Conditions and Privacy Policy.</p>
+            </div>
+        </form>
 
     </div>
   )
