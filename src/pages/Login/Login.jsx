a8b1a511b4cb1b730e45c1f184d79a82e6061fb3
oiiiMansi <sainimansi360@gmail.com>
2025-06-10 11:41:43 +0530

    Design login page with form fields and agreement section

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -3,24 +3,28 @@ import './Login.css'
 import assets from '../../assets/assets'
 
 const Login = () => {
-  return (
-    <div className='login'>
-        <img src={assets.logo_big} alt="" className='logo' />
-        <form className='login-form'>
-            <h2>Sign Up</h2>
-            <input type="text" placeholder='username' className="form-input" required />
-            <input type="email" placeholder='Email address' className="form-input" />
-            <input type="password" placeholder='password' className="form-input" />
-            <button type='submit'>Sign Up</button>
+    return (
+        <div className='login'>
+            <img src={assets.logo_big} alt="" className='logo' />
+            <form className='login-form'>
+                <h2>Sign Up</h2>
+                <input type="text" placeholder='username' className="form-input" required />
+                <input type="email" placeholder='Email address' className="form-input" />
+                <input type="password" placeholder='password' className="form-input" />
+                <button type='submit'>Sign Up</button>
 
-            <div className="login-term">
-                <input type="checkbox" />
-                <p>I agree to the Terms and Conditions and Privacy Policy.</p>
-            </div>
-        </form>
+                <div className="login-term">
+                    <input type="checkbox" />
+                    <p>I agree to the Terms and Conditions and Privacy Policy.</p>
+                </div>
 
-    </div>
-  )
+                <div className="login-forgot">
+
+                </div>
+            </form>
+
+        </div>
+    )
 }
 
 export default Login
