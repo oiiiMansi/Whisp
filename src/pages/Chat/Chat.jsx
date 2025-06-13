62ed1d4e2ab8ee3d5c67cc94dbafa9c77ecaef65
oiiiMansi <sainimansi360@gmail.com>
2025-06-13 11:43:56 +0530

    mount LeftSidebar, ChatBox, and RightSidebar in Chat component

--- src/pages/Chat/Chat.jsx
+++ src/pages/Chat/Chat.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import './Chat.css'
+import ChatBox from '../../components/ChatBox/ChatBox'
+import LeftSidebar from '../../components/LeftSidebar/LeftSidebar'
+import RightSidebar from '../../components/RightSidebar/RightSidebar'
 const Chat = () => {
   return (
     <div className='chat'>
       <div className="chat-container">
-        
+        <LeftSidebar />
+        <ChatBox />
+        <RightSidebar />
       </div>
      
     </div>
