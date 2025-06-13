350bd6594b85a8f09eb5e3624b1ae8d79c507792
oiiiMansi <sainimansi360@gmail.com>
2025-06-13 12:37:48 +0530

    create LeftSidebar component with logo, menu, and search layout

--- src/components/LeftSidebar/LeftSidebar.jsx
+++ src/components/LeftSidebar/LeftSidebar.jsx
@@ -3,7 +3,16 @@ import './LeftSidebar.css'
 
 const LeftSidebar = () => {
   return (
-    <div>
+    <div className='ls'>
+      <div className="ls-top">
+        <div className="ls-nav">
+          <img src={assets.logo} className='logo' alt="" />
+          <div className="menu">
+            <img src={assets.menu_icon} alt="" />
+          </div>
+        </div>
+        <div className="ls-search"></div>
+      </div>
       
     </div>
   )
