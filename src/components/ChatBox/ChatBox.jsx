2fe3a7545cd2fd2e52028c2d49a0e631aaffec92
oiiiMansi <sainimansi360@gmail.com>
2025-06-28 12:40:58 +0530

    style(chat): style sender and receiver profile images with uniform size and rounded shape

--- src/components/ChatBox/ChatBox.jsx
+++ src/components/ChatBox/ChatBox.jsx
@@ -16,27 +16,31 @@ const ChatBox = () => {
           <p className="msg">
             hehehehehe hehehehehe
           </p>
-          <img src={assets.profile_img} alt="" />
-          <p>12:00 PM</p>
+          <div>
+            <img src={assets.profile_img} alt="" />
+            <p>12:00 PM</p>
+          </div>
         </div>
 
         <div className="r-msg">
           <p className="msg">
             hehehehehe hehehehehe
           </p>
-          <img src={assets.profile_img} alt="" />
-          <p>12:00 PM</p>
+          <div>
+            <img src={assets.profile_img} alt="" />
+            <p>12:00 PM</p>
+          </div>
         </div>
       </div>
       <div className="chat-input">
         <input type="text" placeholder='Send a message' />
-        <input type="file" id='image' accept='image/png, image/jpeg' hidden/>
+        <input type="file" id='image' accept='image/png, image/jpeg' hidden />
         <label htmlFor="image">
           <img src={assets.gallery_icon} alt="" />
         </label>
         <img src={assets.send_button} alt="" />
       </div>
-      
+
     </div>
   )
 }
