1df701629a9381b8f5016871c85f6c01e1523d01
oiiiMansi <sainimansi360@gmail.com>
2025-07-02 10:08:09 +0530

    Enhance RightSidebar with media section and logout button

--- src/components/RightSidebar/RightSidebar.jsx
+++ src/components/RightSidebar/RightSidebar.jsx
@@ -11,7 +11,19 @@ const RightSidebar = () => {
         <h3>Billy Butcher <img src={assets.green_dot} alt="" /></h3>
         <p>Hey there, i am using Whisp</p>
       </div>
-      
+      <hr />
+      <div className="rs-media">
+        <p>Media</p>
+        <div>
+          <img src={assets.pic1} alt="" />
+          <img src={assets.pic2} alt="" />
+          <img src={assets.pic3} alt="" />
+          <img src={assets.pic4} alt="" />
+          <img src={assets.pic1} alt="" />
+          <img src={assets.pic2} alt="" />
+        </div>
+      </div>
+      <button>Logout</button>
     </div>
   )
 }
