95e3a4ccee9e4fccc9b7fe57928607624a7f524f
oiiiMansi <sainimansi360@gmail.com>
2025-07-02 10:00:58 +0530

    Add RightSidebar component with user profile display

--- src/components/RightSidebar/RightSidebar.jsx
+++ src/components/RightSidebar/RightSidebar.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import './RightSidebar.css'
+import assets from '../../assets/assets'
+
 
 const RightSidebar = () => {
   return (
-    <div>
+    <div className='rs'>
+      <div className="rs-profile">
+        <img src={assets.profile_img} alt="" />
+        <h3>Billy Butcher <img src={assets.green_dot} alt="" /></h3>
+        <p>Hey there, i am using Whisp</p>
+      </div>
       
     </div>
   )
