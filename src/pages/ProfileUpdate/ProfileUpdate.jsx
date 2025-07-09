9d3c4815d39c3be64743864c8f6010c53afae223
oiiiMansi <sainimansi360@gmail.com>
2025-07-09 10:07:37 +0530

    Add ProfileUpdate component with avatar upload UI

--- src/pages/ProfileUpdate/ProfileUpdate.jsx
+++ src/pages/ProfileUpdate/ProfileUpdate.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import './ProfileUpdate.css'
+import assets from '../../assets/assets';
 const ProfileUpdate = () => {
   return (
-    <div>
-      
+    <div className='profile'>
+      <div className="profile-container">
+        <form>
+          <h3>Profile Details</h3>
+          <label htmlFor="avatar">
+            <input type="file" id='avatar' accept='.png, .jpg, .jpeg' hidden />
+            <img src={assets.avatar_icon} alt="" />
+            Upload Profile image
+          </label>
+          <input type="text" placeholder='Your Name' required/>
+        </form>
+      </div>
     </div>
   )
 }
