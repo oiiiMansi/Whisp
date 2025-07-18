6099c18f10bf1ac55b82b8a96c882b474ca62ee0
oiiiMansi <sainimansi360@gmail.com>
2025-07-18 10:03:03 +0530

    minor update

--- src/pages/ProfileUpdate/ProfileUpdate.jsx
+++ src/pages/ProfileUpdate/ProfileUpdate.jsx
@@ -3,7 +3,7 @@ import './ProfileUpdate.css'
 import assets from '../../assets/assets';
 const ProfileUpdate = () => {
 
-  const [image,setImage] = useState(false);
+  const [image, setImage] = useState(false);
 
   return (
     <div className='profile'>
@@ -11,11 +11,11 @@ const ProfileUpdate = () => {
         <form>
           <h3>Profile Details</h3>
           <label htmlFor="avatar">
-            <input onChange={(e)=>setImage(e.target.files[0])} type="file" id='avatar' accept='.png, .jpg, .jpeg' hidden />
-            <img src={image? URL.createObjectURL(image) :  assets.avatar_icon} alt="" />
+            <input onChange={(e) => setImage(e.target.files[0])} type="file" id='avatar' accept='.png, .jpg, .jpeg' hidden />
+            <img src={image ? URL.createObjectURL(image) : assets.avatar_icon} alt="" />
             Upload Profile image
           </label>
-          <input type="text" placeholder='Your Name' required/>
+          <input type="text" placeholder='Your Name' required />
           <textarea placeholder='Write Profile Bio' required></textarea>
           <button type='submit'>Save</button>
         </form>
