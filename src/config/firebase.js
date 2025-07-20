d908d83edf13d35ac0bf07d83afb1fd613fdb130
oiiiMansi <sainimansi360@gmail.com>
2025-07-20 11:12:41 +0530

    Initialize Firebase with basic configuration

---
+++ src/config/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA3Lw_RlOglMd7sq9nNLzkNUBBrb02UFUg",
+  authDomain: "whisp-b84a4.firebaseapp.com",
+  projectId: "whisp-b84a4",
+  storageBucket: "whisp-b84a4.firebasestorage.app",
+  messagingSenderId: "414338702766",
+  appId: "1:414338702766:web:18a0635f91c6c178153c3f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
