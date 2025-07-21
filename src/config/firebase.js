46a195d687019b22da55eba2cea110cf7ff7a1e5
oiiiMansi <sainimansi360@gmail.com>
2025-07-21 21:41:39 +0530

    minor change

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,8 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyA3Lw_RlOglMd7sq9nNLzkNUBBrb02UFUg",
   authDomain: "whisp-b84a4.firebaseapp.com",
